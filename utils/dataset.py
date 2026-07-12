f2ee7b679f459e8dfdf283d2b2ec55bde77da496
Caz Yokoyama <caz@caztech.com>
2026-07-12 08:43:32 -0700

    youtube-dl is outdated and YouTube aggressively flags automated downloads.
    yt-dlp is installed on Ubuntu as
    curl -fsSL https://deno.land | sh
    sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
    sudo chmod a+rx /usr/local/bin/yt-dlp
    
    Signed-off-by: Caz Yokoyama <caz@caztech.com>

--- utils/dataset.py
+++ utils/dataset.py
@@ -92,7 +92,7 @@ def download_wavs(args):
         
         # Download full video of whatever format
         video_name = os.path.join(audios_dir, '_Y{}.%(ext)s'.format(audio_id))
-        os.system("youtube-dl --quiet -o '{}' -x https://www.youtube.com/watch?v={}"\
+        os.system("yt-dlp --quiet -o '{}' -x https://www.youtube.com/watch?v={}"\
             .format(video_name, audio_id))
 
         video_paths = glob.glob(os.path.join(audios_dir, '_Y' + audio_id + '.*'))
